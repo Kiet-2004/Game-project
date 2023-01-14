c1be9d5b6eac048ebdba579e222926598c304f1d
ibhnim4002 <88875055+ibhnim4002@users.noreply.github.com>
2023-01-14 18:34:27 +0700

    Add files via upload

---
+++ minigame/gungunbang.py
@@ -0,0 +1,106 @@
+import pygame as py
+from random import randint as r
+
+class minigame:
+    def __init__(self):
+        py.init()
+        self.width = 500 * 2
+        self.height = 400 * 2
+        self.fps = 60
+        self.screen = py.display.set_mode((self.width, self.height))
+        self.font = py.font.Font('../assets/font/FreeSansBold.ttf', int(self.height/16))
+        self.cross = py.transform.scale(py.image.load('../assets/minigame/gungunbang/crosshair.png'), (self.width/10, self.height/8))
+        self.tag = py.transform.scale(py.image.load('../assets/minigame/gungunbang/target.png'), (self.width/10, self.height/8))
+        py.mouse.set_visible(False)
+        self.ori_time = py.time.get_ticks()
+        self.score = [0, 0]
+        self.tag_1 =[]
+        self.tag_2 = []
+        self.spawn = False
+        self.spawn_time = 0
+        self.time = 0
+
+    def draw(self):
+        self.screen.fill((255, 255, 255))
+        for pos in self.tag_1 + self.tag_2:
+            self.screen.blit(self.tag, pos)
+        self.screen.blit(self.font.render(str(self.time), True, (0, 0, 0)), self.font.render(str(self.time), True, (0, 0, 0)).get_rect(center = (self.width/2, self.height/10)))
+        self.screen.blit(self.font.render(str(self.score[0]) + ' - ' + str(self.score[1]), True, (0, 0, 0)), self.font.render(str(self.score[0]) + ' - ' + str(self.score[1]), True, (0, 0, 0)).get_rect(center = (self.width/2, self.height/5)))
+        py.draw.line(self.screen, (0, 0, 0), (self.width/2, self.height*3/10), (self.width/2, self.height), width=3)
+        self.screen.blit(self.cross, self.cross.get_rect(center = py.mouse.get_pos()))
+        
+    def spawner(self):
+        self.spawn_time = self.time
+        self.spawn = False
+        for i in range (0, r(1, 5)):
+            for i in range (0, 5):
+                check_1 = True
+                check_2 = True
+                pos = [r(self.width/20, self.width/2 - self.width/20),
+                       r(self.height/16, self.height - self.height/16)]
+                for rect in self.tag_1:
+                    if self.tag.get_rect(center = pos).colliderect(rect):
+                        check_1 = False
+                for rect in self.tag_2:
+                    if self.tag.get_rect(center = (pos[0] + self.width/2, pos[1])).colliderect(rect):
+                        check_2 = False
+                if check_1:
+                    self.tag_1.append(self.tag.get_rect(center = pos))
+                if check_2:                
+                    self.tag_2.append(self.tag.get_rect(center = (pos[0] + self.width/2, pos[1])))                
+                if check_1 or check_2:
+                    break
+                        
+    def timer(self):
+        self.time = int((py.time.get_ticks() - self.ori_time)/1000)
+        if self.time > self.spawn_time:
+                self.spawn = True
+        if self.time % 1 == 0 and self.spawn:
+                self.spawner()
+
+    def shoot(self, pos):
+        count = 0
+        for i in self.tag_1:
+            if abs(i.center[0] - pos[0]) < self.width/20 and abs(i.center[1] - pos[1]) < self.height/16:
+                if abs(i.center[0] - pos[0]) < self.width/100 and abs(i.center[1] - pos[1]) < self.height/80:
+                    self.score[0] += 3
+                elif abs(i.center[0] - pos[0]) < self.width/50 and abs(i.center[1] - pos[1]) < self.height/40:
+                    self.score[0] += 2
+                else:
+                    self.score[0] += 1
+                self.tag_1.pop(count)
+                break
+            count += 1
+        count = 0
+        for i in self.tag_2:
+            if abs(i.center[0] - pos[0]) < self.width/20 and abs(i.center[1] - pos[1]) < self.height/16:
+                if abs(i.center[0] - pos[0]) < self.width/100 and abs(i.center[1] - pos[1]) < self.height/80:
+                    self.score[1] += 3
+                elif abs(i.center[0] - pos[0]) < self.width/50 and abs(i.center[1] - pos[1]) < self.height/40:
+                    self.score[1] += 2
+                else:
+                    self.score[1] += 1
+                self.tag_2.pop(count)
+                break
+            count += 1
+                
+    def run(self):        
+        while True:
+            py.time.Clock().tick(self.fps)
+            self.timer()
+            self.draw()
+            for event in py.event.get():
+                if event.type == py.QUIT:
+                    py.quit()
+                elif event.type == py.MOUSEBUTTONDOWN:
+                    self.shoot(event.pos)
+            if self.time > 40:
+                if self.score[0] > self.score[1]:
+                    return 1
+                elif self.score[1] > self.score[0]:
+                    return 0
+                else:
+                    self.__init__()
+            py.display.update()
+
+print(minigame().run())
---
+++ minigame/sheldon_rps.py
@@ -0,0 +1,86 @@
+import pygame as py
+
+class minigame:
+    def __init__(self):
+        py.init()
+        self.width = 500 * 2
+        self.height = 400 * 2
+        self.fps = 60
+        self.screen = py.display.set_mode((self.width, self.height))
+        self.font = py.font.Font('../assets/font/FreeSansBold.ttf', int(self.height/16))
+        self.rock = py.transform.scale(py.image.load('../assets/minigame/sheldons_rps/rock.png').convert_alpha(), (self.width/5, self.height/2))
+        self.paper = py.transform.scale(py.image.load('../assets/minigame/sheldons_rps/paper.png').convert_alpha(), (self.width/5, self.height/2))
+        self.scissors = py.transform.scale(py.image.load('../assets/minigame/sheldons_rps/scissors.png').convert_alpha(), (self.width/5, self.height/2))
+        self.lizard = py.transform.scale(py.image.load('../assets/minigame/sheldons_rps/lizard.png').convert_alpha(), (self.width/5, self.height/2))
+        self.spock = py.transform.scale(py.image.load('../assets/minigame/sheldons_rps/spock.png').convert_alpha(), (self.width/5, self.height/2))
+        self.rock_rect = self.rock.get_rect(topleft = (0, 0))
+        self.paper_rect = self.paper.get_rect(topleft = (self.width/5, 0))
+        self.scissors_rect = self.scissors.get_rect(topleft = (self.width*2/5, 0))
+        self.lizard_rect = self.lizard.get_rect(topleft = (self.width*3/5, 0))
+        self.spock_rect = self.spock.get_rect(topleft = (self.width*4/5, 0))
+        self.mng = [ [self.rock, self.rock_rect, 0],
+                [self.paper, self.paper_rect, 1],
+                [self.scissors, self.scissors_rect, 2],
+                [self.spock, self.spock_rect, 3],
+                [self.lizard, self.lizard_rect, 4] ]
+        self.choose = []
+        self.turn = 0
+        self.score = [0, 0]
+        self.bruh = False
+
+    def fin(self):
+        self.point = self.choose[0] - self.choose[1]
+        if(self.point == 0):
+            self.bruh = True
+        elif(self.point < 0 and self.point % 2 == 0 or self.point > 0 and self.point % 2 == 1):
+            self.score[0] += 1
+        else:
+            self.score[1] += 1
+        self.turn += 1
+
+    def draw(self):
+        self.screen.fill((255, 255, 255))
+        self.screen.blit(self.rock, self.rock_rect)
+        self.screen.blit(self.paper, self.paper_rect)
+        self.screen.blit(self.scissors, self.scissors_rect)
+        self.screen.blit(self.lizard, self.lizard_rect)
+        self.screen.blit(self.spock, self.spock_rect)
+        self.screen.blit(self.font.render(str(self.score[0]) + ' - ' + str(self.score[1]), True, (0, 0, 0)), self.font.render(str(self.score[0]) + ' - ' + str(self.score[1]), True, (0, 0, 0)).get_rect(center = (self.width/2, self.height*2.3/4)))
+        if (self.turn == 1):
+            self.screen.blit(self.mng[self.choose[0]][0], (self.width/5, self.height/2))
+        elif (self.turn >= 2):
+            self.screen.blit(self.mng[self.choose[0]][0], (self.width/5, self.height/2))
+            self.screen.blit(self.mng[self.choose[1]][0], (self.width*3/5, self.height/2))
+            if (self.score[0] == 3):
+                self.screen.blit(self.font.render("WIN", True, (0, 0, 0)), self.font.render("WIN", True, (0, 0, 0)).get_rect(center = (self.width/10, self.height*3/4)))
+            elif (self.score[1] == 3):
+                self.screen.blit(self.font.render("WIN", True, (0, 0, 0)), self.font.render("WIN", True, (0, 0, 0)).get_rect(center = (self.width*9/10, self.height*3/4)))
+            elif self.bruh:
+                self.screen.blit(self.font.render("DRAW", True, (0, 0, 0)), self.font.render("DRAW", True, (0, 0, 0)).get_rect(center = (self.width/2, self.height*3/4)))
+
+    def run(self):        
+        while True:
+            py.time.Clock().tick(self.fps)
+            for event in py.event.get():
+                if event.type == py.QUIT:
+                    py.quit()
+                elif event.type == py.MOUSEBUTTONDOWN:
+                    if (self.turn == 3):
+                        if self.score[0] == 3:
+                            return 1
+                        elif self.score[1] == 3:
+                            return 0
+                        self.turn = 0
+                        self.bruh = False
+                        self.choose.clear()
+                    else:
+                        for pos in self.mng:
+                            if pos[1].collidepoint(event.pos):
+                                self.turn += 1
+                                self.choose.append(pos[2])
+                                if (self.turn == 2):
+                                    self.fin()
+            self.draw()
+            py.display.update()
+
+print(minigame().run())
